dad61acb6cb83199455e34a45870e7201477f8f6
Caike Laurentino <caike_tb@ymail.com>
2021-03-09 10:47:50 -0300

    Erro no sawsbuck arrumado

--- index.js
+++ index.js
@@ -7,27 +7,29 @@
         var usdValue = Math.round(usdData.rates.BRL * 100)
         document.getElementById('usdValue').innerText = `US$ 1 = R$${(parseFloat(usdData.rates.BRL).toFixed(2))}`
 
-
-        let value_timestamp = usdData.timestamp
-        var date = new Date(value_timestamp * 1000);
-        var hours = date.getHours();
-        var minutes = "0" + date.getMinutes();
-        var seconds = "0" + date.getSeconds();
-
-        
-        document.getElementById('updateTime').innerText = hours + ':' + minutes.substr(-2) + ' BRT';
-
-
-        let pokeUrl = `https://pokeapi.co/api/v2/pokemon/${usdValue}`
-        fetch(pokeUrl)
-          .then(response => response.json())
-          .then(function (pokeData) {
-            document.getElementById('pokeName').innerText = pokeData.name
-            document.getElementById('pokeName').href = `https://pokemondb.net/pokedex/${pokeData.name}`
-            document.getElementById('pokeSprite').src = `https://pokeres.bastionbot.org/images/pokemon/${pokeData.id}.png`
-            document.getElementById('pokeId').innerText = `#${pokeData.id}`
+            let pokeUrl = `https://pokeapi.co/api/v2/pokemon/${usdValue}`
+            fetch(pokeUrl)
+              .then(response => response.json())
+              .then(function (pokeData) {
+                document.getElementById('pokeName').innerText = pokeData.name
+                document.getElementById('pokeName').href = `https://pokemondb.net/pokedex/${pokeData.name}`
+                if (pokeData.id == 586) {
+                  var sawsbuck = Math.floor(Math.random() * 4)
+                  console.log(sawsbuck)
+                  if (sawsbuck == 0) {
+                    document.getElementById('pokeSprite').src = `https://pokeres.bastionbot.org/images/pokemon/586-spring.png`
+                  } else if (sawsbuck == 1) {
+                    document.getElementById('pokeSprite').src = `https://pokeres.bastionbot.org/images/pokemon/586-summer.png`
+                  } else if (sawsbuck == 2) {
+                    document.getElementById('pokeSprite').src = `https://pokeres.bastionbot.org/images/pokemon/586-autumn.png`
+                  } else {
+                    document.getElementById('pokeSprite').src = `https://pokeres.bastionbot.org/images/pokemon/586-winter.png`
+                  }
+                } else {
+                  document.getElementById('pokeSprite').src = `https://pokeres.bastionbot.org/images/pokemon/${pokeData.id}.png`
+                }
+                document.getElementById('pokeId').innerText = `#${pokeData.id}`
+              })
           })
-      })
   }
-
 }
